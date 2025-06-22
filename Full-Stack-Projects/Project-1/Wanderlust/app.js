7dc36acde57c85c6348d04de46fdfec85c3c2168
Zahanboi <zahansharma123@gmail.com>
2025-06-22 22:37:22 +0530

    updating files

--- Full-Stack-Projects/Project-1/Wanderlust/app.js
+++ Full-Stack-Projects/Project-1/Wanderlust/app.js
@@ -8,8 +8,7 @@ const ejsMate = require('ejs-mate')
 // const ExpressError = require("./utils/ExpressError.js");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-
-
+const { listingSchema } = require("./utils/schemaValidate.js")
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -30,6 +29,20 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views/listings"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const validateSchema = (req , res , next) => {
+  // console.log(listingSchema.validate(req.body));
+ 
+  let {error} = listingSchema.validate(req.body);
+
+  if (error) {
+    let errMsg = error.details.map((e) => e.message).join(", "); //keep inside if block otherwise error as no error exist then no errMsg
+    throw new ExpressError(500 , errMsg);
+  }else{
+    next();
+  }
+  
+}
 
 app.get("/" , ((req , res) => {
 res.send("Hello World");
@@ -69,16 +82,12 @@ app.get("/listings/:id", wrapAsync(async (req,res) => {
   res.render("show.ejs" , {listings} );
 }));
 
-app.post("/listings", wrapAsync(async (req, res) => {
+app.post("/listings", validateSchema, wrapAsync(async (req, res) => {
   // const newlisitng = req.body;
   console.log(req.body.listing);
-  if (!req.body.listing) {
-    throw new ExpressError(500 , "invalid data");
-  }
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
-
 }));
 
 app.get("/listings/:id/edit" , wrapAsync(async (req, res) => {
@@ -88,7 +97,7 @@ app.get("/listings/:id/edit" , wrapAsync(async (req, res) => {
 }));
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id", validateSchema, wrapAsync(async (req, res) => {
   let { id } = req.params;
   
   await Listing.findByIdAndUpdate(id, { ...req.body.listing }); //pass by deconstructing the req.body.listing object
@@ -110,7 +119,7 @@ app.all("*", (req, res) => {
 
 app.use((err, req, res, next) => {
   let {statusCode = 500 , message} = err
-  res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs", {message});
   console.log(message);
 });
 
---
+++ Full-Stack-Projects/Project-1/Wanderlust/utils/schemaValidate.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+
+const listingSchema = Joi.object({
+    listing : Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        image: Joi.object().allow("", null),
+        price: Joi.number().min(0).required(),
+        location: Joi.string().required(),
+        country: Joi.string().required()
+    }).required()
+}); 
+
+module.exports = {
+  listingSchema
+};
