bdcebd4e7885221d8304d606b56bfced236687d0
Zahanboi <zahansharma123@gmail.com>
2025-07-18 22:07:00 +0530

    update-7

--- Full-Stack-Projects/Project-2/FaceNest/frontend/vite.config.js
+++ Full-Stack-Projects/Project-2/FaceNest/frontend/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
-
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        // Ensure _redirects is copied to dist/
+        fs.copyFileSync('_redirects', 'dist/_redirects');
+      }
+    }
+  ],
   build: {
     outDir: 'dist'
   }
