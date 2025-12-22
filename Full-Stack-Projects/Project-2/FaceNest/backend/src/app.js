dc231d1bb757e5bf3f085cb764f98bd5085dc3b9
Zahanboi <zahansharma123@gmail.com>
2025-12-22 20:05:41 +0530

    check for workflow

--- Full-Stack-Projects/Project-2/FaceNest/backend/src/app.js
+++ Full-Stack-Projects/Project-2/FaceNest/backend/src/app.js
@@ -26,7 +26,7 @@ const startServer = async () => {
 
     console.log(`Connected to MongoDB ${connectionDb.connection.host}`);
     app.get("/", (req, res) => {
-        res.json({ message: "Welcome to FaceNest API" });
+        res.json({ message: "Welcome to FaceNest API check for aws" });
     });
     server.listen(8003, () => {
         console.log("Server is running on port 8003");
